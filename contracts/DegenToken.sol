d61f15078eaeefc67f62d69eaf723dd63166eb3f
DkRadhey <122290163+DkRadhey@users.noreply.github.com>
2024-07-24 18:45:48 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,91 +1,50 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-contract DegenToken is ERC20,Ownable,ERC20Burnable{
-
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender){}
-
-     // Items name available for Loot
-    enum loot{common,rare,unique,legend}
-
-     struct Player{
-     address toAddress;
-     uint amount;
-
-    }
-
-   //The queue of player for buying degenToken 
-    Player[] public players;
-
-    struct PlayerLoot{
-        
-        uint common;
-        uint rare;
-        uint unique;
-        uint legend;        
-    }
-
-    //To store the redeemed player loot cards
-    mapping (address=>PlayerLoot) public playerCards;
-
-    function buyDegen(address _toAddress,uint _amount)public{
-        players.push(Player({toAddress:_toAddress,amount:_amount}));
-    }
-
-    //minting of tokens for the buyers in the queue
-    function mintToken() public onlyOwner {
-        //loop to mint tokens for buyers in queue
-        while (players.length!=0) {
-            uint i = players.length -1;
-            if (players[i].toAddress != address(0)) { // Check for non-zero address
-            _mint(players[i].toAddress, players[i].amount);
-            players.pop();
-            }
-        }
-    }
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
     
-    //Transfer function for transferring tokens to other player
-    function transferDegen(address _to, uint _amount)public {
-        require(_amount<=balanceOf(msg.sender),"INSUFFICIENT");
-        _transfer(msg.sender, _to, _amount);
+    enum Item { None, ProPlayer, SuperNinja, DegenCap }
+
+    mapping(Item => uint) public itemPrices;
+
+    constructor() ERC20("Degen", "DGN") {
+        itemPrices[Item.ProPlayer] = 200;
+        itemPrices[Item.SuperNinja] = 100;
+        itemPrices[Item.DegenCap] = 75;
     }
 
-
-
-    //Redeem different loot cards
-    function redeemyourloot( loot _card)public{
-        if(_card == loot.common){
-            require(balanceOf(msg.sender)>=15,"INSUFFICIENT");
-            playerCards[msg.sender].common +=1;
-            burn(15);
-        }else if(_card == loot.rare){
-            require(balanceOf(msg.sender)>=25,"Insufficient");
-            playerCards[msg.sender].rare +=1;
-            burn(25);
-        }else if(_card == loot.unique){
-            require(balanceOf(msg.sender)>=35,"INSUFFICIENT");
-            playerCards[msg.sender].unique +=1;
-            burn(35);
-        }else if(_card == loot.legend){
-            require(balanceOf(msg.sender)>=60,"INSUFFICIENT");
-            playerCards[msg.sender].legend +=1;
-            burn(60);
-        }else{
-            revert("NOT AMONGST AVAILABLE CARDS");
-        }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    //function to burn token
-    function burnDegen(address _of, uint amount)public {
-        _burn(_of, amount);
+    function transferTokens(address _receiver, uint amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        transfer(_receiver, amount);
     }
 
-    //function to check the balance of tokens
-    function checkYourBalance()public view returns(uint){
+    function checkBalance() external view returns (uint) {
         return balanceOf(msg.sender);
+    }
+
+    function burnTokens(uint amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+
+    function gameStore() public pure returns (string memory) {
+        return "1. ProPlayer NFT value = 200\n2. SuperNinja value = 100\n3. DegenCap value = 75";
+    }
+
+    function redeemTokens(uint choice) external {
+        require(choice > 0 && choice <= 3, "Invalid selection");
+        Item item = Item(choice);
+        uint price = itemPrices[item];
+        require(balanceOf(msg.sender) >= price, "Insufficient balance");
+        transfer(owner(), price);
     }
 }
